bd75e8106a00f326661de5c9db08474dabadd639
Michelli <contato@michellicristina.com.br>
2021-12-20 19:52:55 -0300

    chore: adiciona camada view loginScreen

---
+++ viewcode-app/Login/Screen/LoginScreen.swift
@@ -0,0 +1,21 @@
+//
+//  LoginScreen.swift
+//  viewcode-app
+//
+//  Created by Michelli Cristina de Paulo Lima on 20/12/21.
+//
+
+import UIKit
+
+class LoginScreen: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
