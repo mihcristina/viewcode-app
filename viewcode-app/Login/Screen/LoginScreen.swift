ec67b198d0bd1ca2a04da2c48ddcbabcf27432d7
Michelli <contato@michellicristina.com.br>
2021-12-20 20:16:03 -0300

    chore: adiciona conexão entre view e viewcontroller

--- viewcode-app/Login/Screen/LoginScreen.swift
+++ viewcode-app/Login/Screen/LoginScreen.swift
@@ -11,7 +11,7 @@ class LoginScreen: UIView {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        
+        self.backgroundColor = .red
     }
     
     required init?(coder: NSCoder) {
--- viewcode-app/Login/ViewController.swift
+++ viewcode-app/Login/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var loginScreen:LoginScreen?
+    
+    override func loadView() {
+        self.loginScreen = LoginScreen()
+        self.view = self.loginScreen
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
