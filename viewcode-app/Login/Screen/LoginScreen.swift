6faa6335155f40654937dd3b49d0e9a48f758388
Michelli <contato@michellicristina.com.br>
2021-12-20 22:00:04 -0300

    chore: adiciona imagem na página de login e define cor da view

--- viewcode-app/Login/Screen/LoginScreen.swift
+++ viewcode-app/Login/Screen/LoginScreen.swift
@@ -9,13 +9,43 @@ import UIKit
 
 class LoginScreen: UIView {
     
+    //elementos visuais da view
+    
+    //imagem
+    lazy var logoAppImageView:UIImageView = {
+       let image = UIImageView()
+        image.translatesAutoresizingMaskIntoConstraints = false
+        image.image = UIImage(named: "Login")
+        image.contentMode = .scaleAspectFit
+        return image
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        self.backgroundColor = .red
+        
+        self.addSubview(self.logoAppImageView)
+        
+        self.backgroundColor = .white
+        
+        setUpConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setUpConstraints(){
+        NSLayoutConstraint.activate([
+            
+            //imagem da logo
+            self.logoAppImageView.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.logoAppImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 60),
+            self.logoAppImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: -60),
+            self.heightAnchor.constraint(equalToConstant: 0.5),
+            
+            
+            
+        ])
     }
     
 }
